b79f64515011d940007c2c554c2d6f8938f7d74e
NKNK-vn <khoi.nkn12@gmail.com>
2022-05-05 09:41:28 +0700

    Add epsilon in LN and replace conv1x1 with dense

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from re import L
 import numpy as np
 import tensorflow as tf
 from keras import Model
@@ -14,23 +15,25 @@ class ConvNeXt:
     def Block(self, filter, x):
         input = x
         x = DepthwiseConv2D(kernel_size=7, padding='same')(x)
-        x = LayerNormalization()(x)
-        x = Conv2D(filters = filter * 4, kernel_size=1 ,padding='same')(x)
-        x = gelu(x)
-        x = Conv2D(filters = filter, kernel_size=1 ,padding='same')(x)
+        x = LayerNormalization(epsilon=1e-6)(x)
+        # x = Conv2D(filters = filter * 4, kernel_size=1 ,padding='same')(x)
+        x = Dense(filter*4)(x)
+        x = Activation('gelu')(x)
+        # x = Conv2D(filters = filter, kernel_size=1 ,padding='same')(x)
+        x = Dense(filter)(x)
         x = Add()([input, x])
         return x
         
     def Downsampling(self):   
         downsample = []
-        stem = [Conv2D(filters = self.channel[0], kernel_size=4, strides=4, name='stem'), LayerNormalization()]
+        stem = [Conv2D(filters = self.channel[0], kernel_size=4, strides=4, name='stem'), LayerNormalization(epsilon=1e-6)]
         downsample.append(stem)
         for i in range(1, 4):
-            downsample.append([LayerNormalization(), Conv2D(filters = self.channel[i], kernel_size=2, strides=2, name='downsample_block_{}'.format(i+1))])
+            downsample.append([LayerNormalization(epsilon=1e-6), Conv2D(filters = self.channel[i], kernel_size=2, strides=2, name='downsample_block_{}'.format(i+1))])
         return downsample
     
     def build_model(self):
-        input = Input([self.img_size, self.img_size, 3])
+        input = Input((self.img_size, self.img_size, 3))
         downsample = self.Downsampling()
         #Block1
         x = downsample[0][0](input)
@@ -45,7 +48,7 @@ class ConvNeXt:
                 x = self.Block(self.channel[i], x)
         #Fully connected
         x = GlobalAveragePooling2D()(x)
-        x = LayerNormalization()(x)
+        x = LayerNormalization(epsilon=1e-6)(x)
         x = Dense(units=self.num_classes, activation='softmax')(x)
         model = Model(input, x)
         return model
